11bbaef49c955993fff13f9f90c06647f58daf2e
Bartłomiej Trzonek <barthek7@gmail.com>
2018-05-15 19:33:38 +0200

    Refactor game ending condition

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ const pS = document.querySelector('.player');
 const Cs = document.querySelector('.computer');
 const buttonNewGame = document.querySelector('.new-game-button');
 
+var playable = false;
 var playerScore = 0;
 var computerScore = 0;
 var numberOfRounds;
@@ -17,59 +18,52 @@ var drawNumber = function () {
 var playerMove = function (event) {
     var computerPlay = drawNumber();
     var playerPlay = this.dataset.move;
-    if (playerScore === numberOfRounds || computerScore === numberOfRounds) {
-        if (playerScore === numberOfRounds) {
-            document.getElementById('number-of-rounds').innerText = numberOfRounds + " " + 'YOU WON THE ENTIRE GAME!';
-        } else {
-            document.getElementById('number-of-rounds').innerText = numberOfRounds + " " + 'YOU LOST THE ENTIRE GAME!';
+
+    if (playable) {
+        switch (playerPlay) {
+            case 'rock':
+                if (computerPlay === ROCK) {
+                    result("DRAW, you picked " + playerPlay.toUpperCase() + " and computer picked ROCK");
+                } else if (computerPlay === SCISSORS) {
+                    result("You WIN, you picked " + playerPlay.toUpperCase() + " and computer picked SCISSORS");
+                    playerScore++;
+                    setScores(playerScore, computerScore);
+                } else {
+                    result("You LOST, you picked " + playerPlay.toUpperCase() + " and computer picked PAPER");
+                    computerScore++;
+                    setScores(playerScore, computerScore);
+                }
+                break;
+            case 'paper':
+                if (computerPlay === ROCK) {
+                    result("You WIN, you picked " + playerPlay.toUpperCase() + " and computer picked ROCK");
+                    playerScore++;
+                    setScores(playerScore, computerScore);
+                } else if (computerPlay === SCISSORS) {
+                    result("You LOST, you picked " + playerPlay.toUpperCase() + " and computer picked SCISSORS");
+                    computerScore++;
+                    setScores(playerScore, computerScore);
+                } else {
+                    result("DRAW, you picked " + playerPlay.toUpperCase() + " and computer picked PAPER");
+                }
+                break;
+            case 'scissors':
+                if (computerPlay === ROCK) {
+                    result("You LOST, you picked " + playerPlay.toUpperCase() + " and computer picked ROCK");
+                    computerScore++;
+                    setScores(playerScore, computerScore);
+                } else if (computerPlay === SCISSORS) {
+                    result("DRAW, you picked " + playerPlay.toUpperCase() + " and computer picked SCISSORS");
+                } else {
+                    result("You WIN, you picked " + playerPlay.toUpperCase() + " and computer picked PAPER");
+                    playerScore++;
+                    setScores(playerScore, computerScore);
+                }
+                break;
         }
 
-
-        return;
     }
-    switch (playerPlay) {
-        case 'rock':
-            if (computerPlay === ROCK) {
-                result("DRAW, you picked " + playerPlay.toUpperCase() + " and computer picked ROCK");
-            } else if (computerPlay === SCISSORS) {
-                result("You WIN, you picked " + playerPlay.toUpperCase() + " and computer picked SCISSORS");
-                playerScore++;
-                setScores(playerScore, computerScore);
-            } else {
-                result("You LOST, you picked " + playerPlay.toUpperCase() + " and computer picked PAPER");
-                computerScore++;
-                setScores(playerScore, computerScore);
-            }
-            break;
-        case 'paper':
-            if (computerPlay === ROCK) {
-                result("You WIN, you picked " + playerPlay.toUpperCase() + " and computer picked ROCK");
-                playerScore++;
-                setScores(playerScore, computerScore);
-            } else if (computerPlay === SCISSORS) {
-                result("You LOST, you picked " + playerPlay.toUpperCase() + " and computer picked SCISSORS");
-                computerScore++;
-                setScores(playerScore, computerScore);
-            } else {
-                result("DRAW, you picked " + playerPlay.toUpperCase() + " and computer picked PAPER");
-            }
-            break;
-        case 'scissors':
-            if (computerPlay === ROCK) {
-                result("You LOST, you picked " + playerPlay.toUpperCase() + " and computer picked ROCK");
-                computerScore++;
-                setScores(playerScore, computerScore);
-            } else if (computerPlay === SCISSORS) {
-                result("DRAW, you picked " + playerPlay.toUpperCase() + " and computer picked SCISSORS");
-            } else {
-                result("You WIN, you picked " + playerPlay.toUpperCase() + " and computer picked PAPER");
-                playerScore++;
-                setScores(playerScore, computerScore);
-            }
-            break;
-
-
-    }
+    endGame();
 };
 
 var result = function (gameResult) {
@@ -82,6 +76,7 @@ var setScores = function (playerScore, computerScore) {
 };
 
 var setNumberOfRounds = function () {
+
     playerScore = 0;
     computerScore = 0;
     setScores(playerScore, computerScore);
@@ -91,6 +86,7 @@ var setNumberOfRounds = function () {
     }
     numberOfRounds = rounds;
     document.getElementById('number-of-rounds').innerText = numberOfRounds;
+    playable = true;
 };
 
 buttonRock.addEventListener('click', playerMove);
@@ -99,4 +95,14 @@ buttonScissors.addEventListener('click', playerMove);
 setScores(playerScore, computerScore);
 buttonNewGame.addEventListener('click', setNumberOfRounds);
 
-
+var endGame = function(){
+    if (playerScore === numberOfRounds || computerScore === numberOfRounds) {
+        if (playerScore === numberOfRounds) {
+            document.getElementById('number-of-rounds').innerText = numberOfRounds + " " + 'YOU WON THE ENTIRE GAME!';
+            playable = false;
+        } else {
+            document.getElementById('number-of-rounds').innerText = numberOfRounds + " " + 'YOU LOST THE ENTIRE GAME!';
+            playable = false;
+        }
+    }
+};
